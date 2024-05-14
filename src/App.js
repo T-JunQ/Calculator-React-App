feb00ecd208ba37aaac53926eccfe9bf4ba279aa
TAN JUN QIN <116868179+T-JunQ@users.noreply.github.com>
2024-05-14 16:13:42 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,265 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { getByDisplayValue, render } from "@testing-library/react";
+import { useEffect, useReducer, useState } from "react";
 
-function App() {
+function Square({ value, onSquareClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
 
-export default App;
+export default function Board() {
+  const [rows, setRows] = useState(3);
+  const [cols, setCols] = useState(3);
+
+  const [currMove, setCurrMove] = useState(0);
+
+  const [xNext, setXNext] = useState(true);
+  const [squares, setSquares] = useState(() => {
+    return Array(rows)
+      .fill(null)
+      .map(() => Array(cols).fill(null)); // Dynamic initialization based on rows and cols
+  });
+  const [winner, setWinner] = useState(false);
+
+  const [history, setHistory] = useState(null);
+
+  function calculateWinner(squares) {
+    if (squares === undefined) {
+      return;
+    }
+
+    for (let i = 0; i < squares.length - 1; i++) {
+      const set = new Set(squares[i]);
+      if (set.size == 1 && !set.has(null)) {
+        console.log("Win");
+        setWinner(true);
+        return;
+      }
+
+      // let colset = new Set();
+      // for (let j = 0; j < squares[i].length; j++) {
+      //   console.log(squares[i][j]);
+      //   colset.add(squares[i][j]);
+      // }
+      // console.log("loop");
+      // console.log(colset);
+      // console.log("Size:", colset.size);
+
+      // if (colset.size == 1 && !colset.has(null)) {
+      //   setWinner(true);
+      //   return;
+      // }
+    }
+
+    for (let i = 0; i < squares[0].length; i++) {
+      let colset = new Set();
+      for (let j = 0; j < squares.length; j++) {
+        console.log(squares[j][i]);
+        colset.add(squares[j][i]);
+      }
+      console.log("loop");
+      console.log(colset);
+      console.log("Size:", colset.size);
+
+      if (colset.size == 1 && !colset.has(null)) {
+        setWinner(true);
+        return;
+      }
+    }
+
+    let diagonalleft = new Set();
+    let diagonalright = new Set();
+    for (let i = 0; i < squares.length; i++) {
+      diagonalleft.add(squares[i][i]);
+      diagonalright.add(squares[i][squares[0].length - 1 - i]);
+    }
+    if (
+      (diagonalleft.size == 1 && !diagonalleft.has(null)) ||
+      (diagonalright.size == 1 && !diagonalright.has(null))
+    ) {
+      setWinner(true);
+      return;
+    }
+    setWinner(false);
+  }
+
+  function onClickListener(i, j) {
+    const newsquares = squares.map((row) => [...row]);
+
+    if (newsquares[i][j] || winner) return;
+
+    newsquares[i][j] = xNext ? "X" : "O";
+    calculateWinner(newsquares);
+
+    let newhistory = [];
+
+    if (history != null) {
+      newhistory = [...history.slice(0, currMove + 1), newsquares];
+      console.log("NotNull");
+    } else {
+      newhistory.push(newsquares);
+    }
+
+    console.log("Old Hisotyr: ", history);
+    console.log("New History: ", newhistory);
+
+    setSquares(newsquares);
+    setXNext(!xNext);
+    setHistory(newhistory);
+    setCurrMove(currMove + 1);
+  }
+
+  function clearBoard() {
+    setWinner(false);
+    let arr = [];
+    for (var i = 0; i < rows; i++) {
+      let array2 = [];
+      for (var j = 0; j < cols; j++) {
+        array2.push(null);
+      }
+      arr.push(array2);
+    }
+    console.log(arr);
+    setSquares(arr);
+    setXNext(true);
+    setHistory(null);
+    setCurrMove(0);
+  }
+
+  function goBack(value, i) {
+    setCurrMove(i);
+    setSquares(value);
+    setXNext((i + 1) % 2 == 0);
+
+    if (currMove != history.length + 1) {
+      setWinner(false);
+    }
+  }
+
+  const moves =
+    history == null ? (
+      ""
+    ) : (
+      <>
+        {history.map((v, i) => (
+          <li>
+            <button key={i + 1} onClick={() => goBack(v, i)}>
+              Game Move #{i + 1}
+            </button>
+          </li>
+        ))}
+      </>
+    );
+
+  return (
+    <>
+      <h1>Tic-Tac-Toe</h1>
+      <div className="game">
+        <div className="game-board">
+          {winner ? (
+            <h1 style={{ color: "blueviolet" }}>Winner: {xNext ? "O" : "X"}</h1>
+          ) : (
+            ""
+          )}
+          {squares.map((col, i) => (
+            <div className="board-row">
+              {col.map((value, j) => (
+                <Square
+                  key={i + j}
+                  value={value}
+                  onSquareClick={() => onClickListener(i, j)}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
+      </div>
+
+      <h5 style={{ marginTop: 10 }}>{xNext ? "X" : "O"}'s turn</h5>
+
+      <button onClick={clearBoard}>Clear Board</button>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          clearBoard();
+        }}
+        style={{ marginTop: 15 }}
+      >
+        <label>Set Number of Rows: </label>
+        <input
+          type="number"
+          onChange={(e) => setRows(e.target.value)}
+          value={rows}
+          required
+        ></input>
+        <br />
+        <label>Set Number of Cols: </label>
+        <input
+          type="number"
+          onChange={(e) => setCols(e.target.value)}
+          value={cols}
+          required
+        ></input>
+        <br />
+        <button type="submit" style={{ marginTop: 2 }}>
+          Apply Changes
+        </button>
+      </form>
+    </>
+  );
+}
+
+// return () => {
+//   rows.forEach((i) => {
+//     <h1>e</h1>;
+//     cols.forEach((j) => {
+//       return (
+//         <>
+//           <h1>e</h1> <Square value={rows[i][j]} />
+//         </>
+//       );
+//     });
+//   });
+// };
+
+// return Array(rows).fill(
+//   <div className="board-row">
+//     {Array(rows)
+//       .fill()
+//       .map((v, i) => (
+//         <Square></Square>
+//       ))}
+//   </div>
+// );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/Calculator.js
@@ -0,0 +1,283 @@
+import styles from "./Calculator.css";
+import { getByDisplayValue, render } from "@testing-library/react";
+import { useEffect, useReducer, useState } from "react";
+import { useInRouterContext } from "react-router-dom";
+import { e, isOperatorNode, round } from "mathjs";
+
+// paranthesis
+// histoyr
+// keypress
+
+export default function Calculator() {
+  const operators = ["+", "-", "*", "/", "%"];
+
+  const calculatorLayout = [
+    ["C", "AC", "%", "/"],
+    ["1", "2", "3", "+"],
+    ["4", "5", "6", "-"],
+    ["7", "8", "9", "*"],
+    ["0", ".", "Enter"],
+  ];
+
+  const [inputVal, setInputVal] = useState("");
+  const [isOutput, setIsOutput] = useState(false);
+  const [history, setHistory] = useState([]);
+
+  function handleNumInput(val) {
+    let isValidInput = true;
+
+    if (operators.includes(val)) {
+      if (isNaN(Number(inputVal[inputVal.length - 1]))) {
+        isValidInput = false;
+      }
+    }
+
+    if (val === ".") {
+      if (isNaN(Number(inputVal[inputVal.length - 1]))) {
+        isValidInput = false;
+      }
+
+      let operatorIndex = -1;
+      operators.forEach((operator) => {
+        let index = inputVal.lastIndexOf(operator);
+        if (index !== -1 && index <= inputVal.length) {
+          operatorIndex = index;
+        }
+      });
+
+      if (operatorIndex !== -1) {
+        const num = inputVal.slice(inputVal.indexOf(operatorIndex));
+        console.log("num", num);
+        if (num.includes(".")) {
+          isValidInput = false;
+        }
+      } else if (inputVal.includes(".")) {
+        isValidInput = false;
+      }
+    }
+
+    if (isValidInput) {
+      if (inputVal === "Undefined" || inputVal === "Infinity") {
+        setInputVal(val);
+      } else {
+        setInputVal(inputVal + val);
+      }
+    }
+  }
+
+  function handleEnter() {
+    var operatorsInInput = [];
+    let allowCalculations = true;
+
+    for (let i = 0; i < inputVal.length; i++) {
+      const char = inputVal[i];
+
+      if (operators.includes(char)) {
+        operatorsInInput.push(char);
+
+        if (inputVal.endsWith(char)) {
+          allowCalculations = false;
+        }
+      }
+    }
+
+    if (operatorsInInput.length !== 0 && allowCalculations) {
+      const numbersInInput = splitByList(inputVal, operators);
+      var output = numbersInInput[0];
+      for (let i = 1; i < numbersInInput.length; i++) {
+        console.log(numbersInInput);
+        output = calculate(output, numbersInInput[i], operatorsInInput[i - 1]);
+        console.log("Output:", output, i);
+      }
+
+      let newHistory = history.slice();
+      newHistory.push(inputVal);
+      setHistory(newHistory);
+
+      setInputVal(
+        output != null || output != undefined
+          ? round(Number(output), 5).toString()
+          : "Undefined"
+      );
+
+      setIsOutput(true);
+    }
+  }
+
+  function splitByList(string, delimiters) {
+    var splitString = string + "";
+    var output = "";
+
+    for (let i = 0; i < splitString.length; i++) {
+      const char = splitString[i];
+      if (operators.includes(char)) {
+        output += ",";
+      } else {
+        output += char;
+      }
+    }
+
+    return output.split(",");
+  }
+
+  function calculate(a, b, operator) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "/":
+        return a / b;
+      case "*":
+        return a * b;
+      case "%":
+        return a % b;
+      default:
+        return null;
+    }
+  }
+
+  function handleAC() {
+    setInputVal("");
+  }
+
+  function handleC() {
+    if (inputVal == "Undefined" || inputVal == "Infinity") {
+      setInputVal("");
+    } else if (inputVal.length > 1) {
+      const newInput = inputVal.slice(0, -1);
+      setInputVal(newInput);
+    } else {
+      setInputVal("");
+    }
+  }
+
+  function handleInputs(input) {
+    console.log(input);
+
+    console.log();
+
+    if (
+      calculatorLayout.toString().split(",").includes(input) ||
+      input === "Backspace"
+    ) {
+      switch (input) {
+        case "Enter":
+          handleEnter();
+          break;
+        case "AC":
+          handleAC();
+          break;
+        case "Backspace":
+          handleC();
+          break;
+        case "C":
+          handleC();
+          break;
+        default:
+          if (isOutput) {
+            console.log(isOutput);
+            setInputVal("");
+            setIsOutput(false);
+          }
+          handleNumInput(input);
+      }
+    }
+  }
+
+  function historyBack(value) {
+    setInputVal(value);
+  }
+
+  function clearHistory() {
+    var arr = [];
+    setHistory(arr);
+  }
+
+  useEffect(() => {
+    function handleKeyDown(event) {
+      handleInputs(event.key);
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  return (
+    <>
+      <h1>Calculator</h1>
+      <div className="calFlex">
+        <table className="cal">
+          <tbody>
+            <tr>
+              <td colSpan={4} className="displayInput">
+                {inputVal}
+              </td>
+            </tr>
+
+            {calculatorLayout.map((row, i) => (
+              <tr key={i}>
+                {row.map((value, j) => (
+                  <td
+                    key={j}
+                    {...(value === "Enter"
+                      ? {
+                          colSpan: 2,
+                        }
+                      : {})}
+                  >
+                    <button
+                      onClick={() => {
+                        handleInputs(value);
+                      }}
+                    >
+                      {value}
+                    </button>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {history.length === 0 ? (
+          ""
+        ) : (
+          <table className="history">
+            <tbody>
+              <tr>
+                <th>History</th>
+              </tr>
+
+              <button
+                onClick={clearHistory}
+                style={{
+                  marginBottom: 10,
+                  marginTop: 5,
+                  padding: 5,
+                  backgroundColor: "#003399",
+                  color: "white",
+                }}
+              >
+                Clear History
+              </button>
+
+              {history.map((v, i) => (
+                <tr>
+                  <td className="historyButton">
+                    <button onClick={() => historyBack(v)}>{v}</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
+}
